49b5e1b311a816b724ed311ccd8767721c942cae
gopalakrishna97 <gopalakrishna9101997@gmail.com>
2023-04-04 16:31:02 +0530

    deployment commit

--- airflow/dags/training_pipeline.py
+++ airflow/dags/training_pipeline.py
@@ -13,7 +13,7 @@ with DAG(
     # [END default_args]
     description='Flight Fare Prediction',
     schedule_interval="@weekly",
-    start_date=pendulum.datetime(2023, 4, 3, tz="UTC"),
+    start_date=pendulum.datetime(2023, 4, 2, tz="UTC"),
     catchup=False,
     tags=['example']
 ) as dag:
